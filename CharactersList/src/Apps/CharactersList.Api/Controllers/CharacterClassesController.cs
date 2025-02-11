fd34efa8f485923cdb82e8684c6a27e08efb6531
Christophe 'DrakeXorn' Bernard <chbernard4444@hotmail.com>
2025-02-11 23:00:54 +0100

    refactor: Remove unnecessary obj files

--- CharactersList/src/Apps/CharactersList.Api/Controllers/CharacterClassesController.cs
+++ CharactersList/src/Apps/CharactersList.Api/Controllers/CharacterClassesController.cs
@@ -15,141 +15,143 @@ namespace CharactersList.Api.Controllers;
 [ApiVersion(1)]
 public class CharacterClassesController: ControllerBase
 {
-    private readonly ICharacterClassRepository _characterClassRepository;
-    private readonly ICharacterRepository _characterDatabaseService;
-    
-    public CharacterClassesController(ICharacterClassRepository characterClassRepository, ICharacterRepository characterDatabaseService)
-    {
-        _characterClassRepository = characterClassRepository;
-        _characterDatabaseService = characterDatabaseService;
-    }
-    
-    [HttpGet]
-    public async Task<List<CharacterClassDto>> Get()
-    {
-        IEnumerable<CharacterClass> classes = await _characterClassRepository.GetCharacterClassesAsync();
+	private readonly ICharacterClassRepository _characterClassRepository;
+	private readonly ICharacterRepository _characterDatabaseService;
 
-        return classes.Select(
-            charClass => new CharacterClassDto 
-            {
-                Id = charClass.Id,
-                Name = charClass.Name,
-                Description = charClass.Description,
-                MaxHealth = charClass.MaxHealth
-            }
-        ).ToList();
-    }
-    
-    [HttpGet("{id:length(24)}")]
-    public async Task<CharacterClassDto?> Get(string id)
-    {
-        CharacterClass? characterClass = await _characterClassRepository.GetCharacterClassAsync(id);
-        
-        if (characterClass is null)
-        {
-            return null;
-        }
+	public CharacterClassesController(ICharacterClassRepository characterClassRepository, ICharacterRepository characterDatabaseService)
+	{
+		_characterClassRepository = characterClassRepository;
+		_characterDatabaseService = characterDatabaseService;
+	}
 
-        return new CharacterClassDto
-        {
-            Id = characterClass.Id,
-            Name = characterClass.Name,
-            Description = characterClass.Description,
-            MaxHealth = characterClass.MaxHealth
-        };
-    }
-    
-    [HttpGet("{id:length(24)}/Characters")]
-    public async Task<List<CharacterDto>> GetCharactersWithClassId(string id)
-    {
-        CharacterClass? characterClass = await _characterClassRepository.GetCharacterClassAsync(id);
-        
-        if (characterClass is null)
-        {
-            return [];
-        }
+	[HttpGet]
+	public async Task<List<CharacterClassDto>> Get()
+	{
+		IEnumerable<CharacterClass> classes = await _characterClassRepository.GetCharacterClassesAsync();
 
-        IEnumerable<Character> characters = await _characterDatabaseService.GetCharactersAsync();
+		return classes.Select(
+			charClass => new CharacterClassDto
+			{
+				Id = charClass.Id,
+				Name = charClass.Name,
+				Description = charClass.Description,
+				MaxHealth = charClass.MaxHealth
+			}
+		).ToList();
+	}
 
-        return
-            characters.Select(
-                character => new CharacterDto
-                {
-                    Class = new MinimalCharacterClassDto
-                    {
-                        Id = characterClass.Id,
-                        Name = characterClass.Name,
-                    },
-                    Id = character.Id,
-                    Name = character.Name,
-                    Health = new HealthDto
-                    {
-                        Current = character.CurrentHealth,
-                        Max = characterClass.MaxHealth
-                    },
-                    UserId = character.UserId,
-                    DateOfBirth = character.DateOfBirth,
-                    Experience = character.Experience,
-                    Gold = character.Gold
-                }
-            ).ToList();
-    }
-    
-    [HttpPost]
-    [Authorize(Policy = "admin")]
-    public async Task<IActionResult> Create(CharacterClassCreationDto characterClass)
-    {
-        CharacterClass createdClass = await _characterClassRepository.CreateCharacterClassAsync(
-            new CharacterClass
-            {
-                Name = characterClass.Name,
-                Description = characterClass.Description,
-                MaxHealth = characterClass.MaxHealth
-            }
-        );
-        
-        return CreatedAtAction(nameof(Get), new { id = createdClass.Id }, createdClass);
-    }
+	[HttpGet("{id:length(24)}")]
+	public async Task<CharacterClassDto?> Get(string id)
+	{
+		CharacterClass? characterClass = await _characterClassRepository.GetCharacterClassAsync(id);
 
-    [HttpPut("{id:length(24)}")]
-    [Authorize(Policy = "admin")]
-    public async Task<IActionResult> Update(string id, CharacterClassUpdateDto update)
-    {
-        CharacterClass? existingCharacterClass = await _characterClassRepository.GetCharacterClassAsync(id);
-        
-        if (existingCharacterClass is null)
-        {
-            return NotFound();
-        }
-        
-        await _characterClassRepository.UpdateCharacterClassAsync(
-            id,
-            new CharacterClass
-            {
-                Id = id,
-                Name = update.Name ?? existingCharacterClass.Name,
-                Description = update.Description ?? existingCharacterClass.Description,
-                MaxHealth = update.MaxHealth ?? existingCharacterClass.MaxHealth
-            }
-        );
-        
-        return NoContent();
-    }
-    
-    [HttpDelete("{id:length(24)}")]
-    [Authorize(Policy = "admin")]
-    public async Task<IActionResult> Delete(string id)
-    {
-        CharacterClass? characterClass = await _characterClassRepository.GetCharacterClassAsync(id);
-        
-        if (characterClass is null)
-        {
-            return NotFound();
-        }
-        
-        await _characterDatabaseService.DeleteCharactersAsync(character => character.ClassId == id);
-        await _characterClassRepository.DeleteCharacterClassAsync(id);
-        
-        return NoContent();
-    }
+		if (characterClass is null)
+		{
+			return null;
+		}
+
+		return new CharacterClassDto
+		{
+			Id = characterClass.Id,
+			Name = characterClass.Name,
+			Description = characterClass.Description,
+			MaxHealth = characterClass.MaxHealth
+		};
+	}
+
+	[HttpGet("{id:length(24)}/Characters")]
+	public async Task<List<CharacterDto>> GetCharactersWithClassId(string id)
+	{
+		CharacterClass? characterClass = await _characterClassRepository.GetCharacterClassAsync(id);
+
+		if (characterClass is null)
+		{
+			return [];
+		}
+
+		IEnumerable<Character> characters = await _characterDatabaseService.GetCharactersAsync(
+			character => character.ClassId == id
+		);
+
+		return
+			characters.Select(
+				character => new CharacterDto
+				{
+					Class = new MinimalCharacterClassDto
+					{
+						Id = characterClass.Id,
+						Name = characterClass.Name,
+					},
+					Id = character.Id,
+					Name = character.Name,
+					Health = new HealthDto
+					{
+						Current = character.CurrentHealth,
+						Max = characterClass.MaxHealth
+					},
+					UserId = character.UserId,
+					DateOfBirth = character.DateOfBirth,
+					Experience = character.Experience,
+					Gold = character.Gold
+				}
+			).ToList();
+	}
+
+	[HttpPost]
+	[Authorize(Policy = "admin")]
+	public async Task<IActionResult> Create(CharacterClassCreationDto characterClass)
+	{
+		CharacterClass createdClass = await _characterClassRepository.CreateCharacterClassAsync(
+			new CharacterClass
+			{
+				Name = characterClass.Name,
+				Description = characterClass.Description,
+				MaxHealth = characterClass.MaxHealth
+			}
+		);
+
+		return CreatedAtAction(nameof(Get), new { id = createdClass.Id }, createdClass);
+	}
+
+	[HttpPut("{id:length(24)}")]
+	[Authorize(Policy = "admin")]
+	public async Task<IActionResult> Update(string id, CharacterClassUpdateDto update)
+	{
+		CharacterClass? existingCharacterClass = await _characterClassRepository.GetCharacterClassAsync(id);
+
+		if (existingCharacterClass is null)
+		{
+			return NotFound();
+		}
+
+		await _characterClassRepository.UpdateCharacterClassAsync(
+			id,
+			new CharacterClass
+			{
+				Id = id,
+				Name = update.Name ?? existingCharacterClass.Name,
+				Description = update.Description ?? existingCharacterClass.Description,
+				MaxHealth = update.MaxHealth ?? existingCharacterClass.MaxHealth
+			}
+		);
+
+		return NoContent();
+	}
+
+	[HttpDelete("{id:length(24)}")]
+	[Authorize(Policy = "admin")]
+	public async Task<IActionResult> Delete(string id)
+	{
+		CharacterClass? characterClass = await _characterClassRepository.GetCharacterClassAsync(id);
+
+		if (characterClass is null)
+		{
+			return NotFound();
+		}
+
+		await _characterDatabaseService.DeleteCharactersAsync(character => character.ClassId == id);
+		await _characterClassRepository.DeleteCharacterClassAsync(id);
+
+		return NoContent();
+	}
 }
